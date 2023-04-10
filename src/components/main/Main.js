e8de576ab773f9eb4b5a98dbba173348eb3d9f49
Frank Böker <boeker.frank@gmail.com>
2023-04-10 08:50:15 +0200

    add TabBar - Button

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,10 +1,12 @@
 import EntryForm from "./EntryForm.js";
+import TabBar from "./TabBar.js";
 
 export default function Main() {
   return (
     <section>
       <h2>Main</h2>
       <EntryForm />
+      <TabBar />
     </section>
   );
 }
---
+++ src/components/main/TabBar.js
@@ -0,0 +1,8 @@
+export default function TabBar() {
+  return (
+    <section>
+      <button type="button">All Entries</button>
+      <button type="button">Favorites</button>
+    </section>
+  );
+}
