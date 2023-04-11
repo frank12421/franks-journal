6cd7798b19280b9f6f4d96e39a280693f7e152cf
Frank Böker <boeker.frank@gmail.com>
2023-04-11 09:10:10 +0200

    change Card-Datta to array-entrie

--- src/components/main/CardEntryList.js
+++ src/components/main/CardEntryList.js
@@ -2,21 +2,17 @@ import "./CardEntryList.css";
 import { ReactComponent as FavLogo } from "../../resources/star.svg";
 // import { ReactComponent as FavLogoFilled } from "../../resources/star-filled.svg";
 
-export default function CardEntryList() {
+export default function CardEntryList({ date, motto, notes }) {
   return (
     <article className="Card-EntryList">
-      <h4 className="Card-EntryList--Date">APR 10, 2023</h4>
+      <h4 className="Card-EntryList--Date">{date}</h4>
       <div className="Card-Entry--headandFavorite">
-        <h3>"That#s life in the City"</h3>
+        <h3>{motto}</h3>
         <button type="button" className="Card-EntryList--FavButton">
           <FavLogo />
         </button>
       </div>
-      <p className="Card-EntryList-text">
-        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
-        eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
-        voluptua. At vero eos et accusam et
-      </p>
+      <p className="Card-EntryList-text">{notes}</p>
     </article>
   );
 }
